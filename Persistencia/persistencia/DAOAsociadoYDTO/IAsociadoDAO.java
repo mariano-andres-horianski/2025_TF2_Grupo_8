ec6ce71ac565b72ba0f5dcf22f9b088c18ce6eef
Jazmin <jazzy.p.toi@gmail.com>
2025-11-10 23:41:05 -0300

    merge de persistencia y devF-branch

--- Persistencia/persistencia/DAOAsociadoYDTO/IAsociadoDAO.java
+++ Persistencia/persistencia/DAOAsociadoYDTO/IAsociadoDAO.java
@@ -3,6 +3,13 @@ package persistencia.DAOAsociadoYDTO;
 import java.util.HashMap;
 import persistencia.Excepciones.*;
 
+/**
+ * Define la interfaz del Data Access Object (DAO) para AsociadoDTO.
+ * 
+ * <p>Esta interfaz abstrae las operaciones CRUD (Create, Read, Update, Delete)
+ * sobre la entidad <b>Asociado</b> en el sistema de persistencia.
+ */
+
 public interface IAsociadoDAO {
     HashMap<String, AsociadoDTO> obtenerTodosMap();
     void agregar(AsociadoDTO a) throws DatoInvalidoException, AsociadoExistenteException;
